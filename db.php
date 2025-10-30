600d1e41b45c890535a715abaffa798328c87fac
Rainshi86 <160769313+Rainshi86@users.noreply.github.com>
2025-10-31 05:09:18 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,16 +1,31 @@
 <?php
-$host = 'localhost';  // Host
-$dbname = 'rentroomdb';  // Database name
-$usernamedb = 'root';  // Database username
-$password = '';  // Database password
+
+<?php
+$host = 'your-railway-host';  // จาก Railway
+$dbname = 'your-railway-dbname';
+$usernamedb = 'your-railway-username';
+$password = 'your-railway-password';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $usernamedb, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $usernamedb, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "เชื่อมต่อฐานข้อมูลสำเร็จ"; // เพิ่มข้อความนี้เพื่อตรวจสอบ
+    // echo "เชื่อมต่อฐานข้อมูลสำเร็จ";
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
     die();
 }
-
 ?>
+
+// $host = 'localhost';  // Host
+// $dbname = 'rentroomdb';  // Database name
+// $usernamedb = 'root';  // Database username
+// $password = '';  // Database password
+
+// try {
+//     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $usernamedb, $password);
+//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//     // echo "เชื่อมต่อฐานข้อมูลสำเร็จ"; // เพิ่มข้อความนี้เพื่อตรวจสอบ
+// } catch (PDOException $e) {
+//     echo "Connection failed: " . $e->getMessage();
+//     die();
+// }
