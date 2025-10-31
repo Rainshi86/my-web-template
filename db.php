ff58ecbbfdf22370e701bed9c8d7c686ae967b56
niphon <niphon.iamphrom@gmail.com>
2025-10-31 17:01:27 +0700

    update db.php for railway

--- db.php
+++ db.php
@@ -1,18 +1,15 @@
 <?php
+$host = getenv('MYSQLHOST');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQLPASSWORD');
+$db   = getenv('MYSQLDATABASE');
+$port = getenv('MYSQLPORT');
 
-$host = 'mysql.railway.internal';  // จาก Railway
-$dbname = '';
-$usernamedb = 'root';
-$password = '';
-
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $usernamedb, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "เชื่อมต่อฐานข้อมูลสำเร็จ";
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-    die();
+$conn = new mysqli($host, $user, $pass, $db, $port);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+// echo "Connected successfully";
 ?>
 
 // $host = 'localhost';  // Host
