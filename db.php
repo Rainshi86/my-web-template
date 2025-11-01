d1aa4684437fd4e6b6e5d320c4d20818fe52aafb
Rainshi <niphon.iamphrom@gmail.com>
2025-11-01 14:55:31 +0700

    remove import.php & edit db.php

--- db.php
+++ db.php
@@ -1,29 +1,30 @@
 <?php
-$host = getenv('MYSQLHOST');
-$user = getenv('MYSQLUSER');
-$pass = getenv('MYSQLPASSWORD');
-$db   = getenv('MYSQLDATABASE');
-$port = getenv('MYSQLPORT');
-
-try {
-    $conn = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $pass);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Connected successfully";
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
-}
-?>
-
-// $host = 'localhost';  // Host
-// $dbname = 'rentroomdb';  // Database name
-// $usernamedb = 'root';  // Database username
-// $password = '';  // Database password
+// $host = getenv('MYSQLHOST');
+// $user = getenv('MYSQLUSER');
+// $pass = getenv('MYSQLPASSWORD');
+// $db   = getenv('MYSQLDATABASE');
+// $port = getenv('MYSQLPORT');
 
 // try {
-//     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $usernamedb, $password);
-//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//     // echo "เชื่อมต่อฐานข้อมูลสำเร็จ"; // เพิ่มข้อความนี้เพื่อตรวจสอบ
+//     $conn = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $pass);
+//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//     // echo "Connected successfully";
 // } catch (PDOException $e) {
-//     echo "Connection failed: " . $e->getMessage();
-//     die();
+//     die("Connection failed: " . $e->getMessage());
 // }
+
+
+$host = 'localhost';  // Host
+$dbname = 'rentroomdb';  // Database name
+$usernamedb = 'root';  // Database username
+$password = '';  // Database password
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $usernamedb, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "เชื่อมต่อฐานข้อมูลสำเร็จ"; // เพิ่มข้อความนี้เพื่อตรวจสอบ
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    die();
+}
+?>
